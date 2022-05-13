2406c258a84ea51be436a232ea160410e10e45c4
aaaa <aaaa>
2022-05-13 11:50:09 +0900

    Initial Commit

---
+++ TapWeight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TapWeight
+//
+//  Created by nnname on 2022/05/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapWeight/TapWeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapWeightApp.swift
+//  TapWeight
+//
+//  Created by nnname on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct TapWeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
