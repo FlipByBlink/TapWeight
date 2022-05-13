f91cd1a539ff589c3aa79123323db042bbf12bce
aaaa <aaaa>
2022-05-13 13:58:49 +0900

    Add Document

---
+++ TapWeight/MenuView.swift
@@ -0,0 +1,140 @@
+
+import SwiftUI
+
+
+struct MenuButton: View { // ⚙️
+    
+    @State private var 🚩Menu: Bool = false
+    
+    @AppStorage("AbleBodyFat") var 🚩BodyFat: Bool = false
+    
+    @AppStorage("LaunchHealthAppAfterLog") var 🚩LaunchHealthAppAfterLog: Bool = false
+    
+    @AppStorage("Unit") var 🛠Unit: 🄴numUnit = .kg
+    
+    var body: some View {
+        Button {
+            🚩Menu = true
+        } label: {
+            Image(systemName: "gear")
+                .font(.largeTitle)
+                .foregroundStyle(.secondary)
+                .foregroundColor(.pink)
+                .padding(22)
+        }
+        .accessibilityLabel("Document")
+        .sheet(isPresented: $🚩Menu) {
+            
+            NavigationView {
+                List {
+                    Section {
+                        Toggle(isOn: $🚩BodyFat) {
+                            Label("🌏Body fat percentage", systemImage: "percent")
+                        }
+                        
+                        Toggle(isOn: $🚩LaunchHealthAppAfterLog) {
+                            Label("🌏Show \"Health\" app after log", systemImage: "arrowshape.turn.up.right")
+                        }
+                        
+                        Picker(selection: $🛠Unit) {
+                            ForEach(🄴numUnit.allCases, id: \.self) { 🏷 in
+                                Text(🏷.rawValue)
+                            }
+                        } label: {
+                            Label("🌏Unit", systemImage: "scalemass")
+                        }
+                    } header: {
+                        Text("🌏Option")
+                    }
+                    
+                    
+                    Section {
+                        Link(destination: URL(string: "x-apple-health://")!) {
+                            HStack {
+                                Label("🌏Open Apple \"Health\" app", systemImage: "heart")
+                                
+                                Spacer()
+                                
+                                Image(systemName: "arrow.up.forward.app")
+                            }
+                            .font(.body.bold())
+                        }
+                    }
+                    
+                    NavigationLink {
+                        📄Document()
+                    } label: {
+                        Label("Document", systemImage: "doc")
+                    }
+                }
+                .navigationTitle("🌏TapWeight")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            🚩Menu = false
+                        } label: {
+                            Image(systemName: "chevron.down")
+                                .foregroundStyle(.secondary)
+                                .grayscale(1.0)
+                                .padding(8)
+                        }
+                        .accessibilityLabel("🌏Dismiss")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct 📄Document: View {
+    var body: some View {
+        List {
+            Section {
+                let 🔗 = "https://apps.apple.com/developer/id1347562710"
+                Link(destination: URL(string: 🔗)!) {
+                    HStack {
+                        Label("Open AppStore page", systemImage: "link")
+                        
+                        Spacer()
+                        
+                        Image(systemName: "arrow.up.forward.app")
+                    }
+                }
+                .font(.subheadline)
+                
+                Text("""
+                    about text
+                    """)
+                .font(.subheadline)
+                .padding(8)
+                
+                Label("version 1.0", systemImage: "signpost.left")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            } header: {
+                Text("About")
+            }
+            
+            Section {
+                Text("""
+                    2022-05-13
+                    (English)This application don't collect user infomation.
+                    (Japanese)このアプリ自身において、ユーザーの情報を一切収集しません。
+                    """)
+                .font(.subheadline)
+                .padding(8)
+            } header: {
+                Text("Privacy Policy")
+            }
+            
+            NavigationLink {
+                Text("placeholder") //📓SourceCodeDoc()
+            } label: {
+                Label("Source code", systemImage: "doc")
+            }
+            .font(.subheadline)
+        }
+        .navigationTitle("Document")
+    }
+}
--- TapWeight/TapWeightApp.swift
+++ TapWeight/TapWeightApp.swift
@@ -5,82 +5,12 @@ import HealthKit
 
 @main
 struct TapWeightApp: App {
-    
-    @State private var 🚩Menu: Bool = false
-    
-    @AppStorage("AbleBodyFat") var 🚩BodyFat: Bool = false
-    
-    @AppStorage("LaunchHealthAppAfterLog") var 🚩LaunchHealthAppAfterLog: Bool = false
-    
-    @AppStorage("Unit") var 🛠Unit: 🄴numUnit = .kg
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .statusBar(hidden: true)
                 .overlay(alignment: .bottomTrailing) {
-                    Button {
-                        🚩Menu = true
-                    } label: {
-                        Image(systemName: "gear")
-                            .font(.largeTitle)
-                            .foregroundStyle(.secondary)
-                            .foregroundColor(.pink)
-                            .padding(22)
-                    }
-                }
-                .popover(isPresented: $🚩Menu) {
-                    NavigationView {
-                        List {
-                            Section {
-                                Toggle(isOn: $🚩BodyFat) {
-                                    Label("🌏Body fat percentage", systemImage: "percent")
-                                }
-                                
-                                Toggle(isOn: $🚩LaunchHealthAppAfterLog) {
-                                    Label("🌏Show \"Health\" app after log", systemImage: "arrowshape.turn.up.right")
-                                }
-                                
-                                Picker(selection: $🛠Unit) {
-                                    ForEach(🄴numUnit.allCases, id: \.self) { 🏷 in
-                                        Text(🏷.rawValue)
-                                    }
-                                } label: {
-                                    Label("🌏Unit", systemImage: "scalemass")
-                                }
-                            } header: {
-                                Text("🌏Option")
-                            }
-                            
-                            
-                            Section {
-                                Link(destination: URL(string: "x-apple-health://")!) {
-                                    HStack {
-                                        Label("🌏Open Apple \"Health\" app", systemImage: "heart")
-                                        
-                                        Spacer()
-                                        
-                                        Image(systemName: "arrow.up.forward.app")
-                                    }
-                                    .font(.body.bold())
-                                }
-                            }
-                        }
-                        .navigationTitle("🌏TapWeight")
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarTrailing) {
-                                Button {
-                                    🚩Menu = false
-                                } label: {
-                                    Image(systemName: "chevron.down")
-                                        .foregroundStyle(.secondary)
-                                        .grayscale(1.0)
-                                        .padding(8)
-                                }
-                                .accessibilityLabel("🌏Dismiss")
-                            }
-                        }
-                    }
+                    MenuButton()
                 }
         }
     }
