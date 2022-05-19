2c940c54102b94bddc5bb43a5ef3818ff9edbcd8
aaaa <aaaa>
2022-05-19 14:22:04 +0900

    a

---
+++ TapWeight/AD.swift
@@ -0,0 +1,20 @@
+//
+//  AD.swift
+//  TapWeight
+//
+//  Created by nnname on 2022/05/19.
+//
+
+import SwiftUI
+
+struct AD: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AD_Previews: PreviewProvider {
+    static var previews: some View {
+        AD()
+    }
+}
