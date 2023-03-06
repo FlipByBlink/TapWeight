91d6f068ba7974d038199cfe29ee3b59ea7f4205
aaaaa <aaaaa>
2023-03-06 19:02:28 +0900

    バックグラウンド起動した際に、広告の起動回数カウントしないように実装変更

--- TapWeight/🧩Sub/📣ADSheet.swift
+++ TapWeight/🧩Sub/📣ADSheet.swift
@@ -2,15 +2,18 @@ import SwiftUI
 
 struct 📣ADSheet: ViewModifier {
     @EnvironmentObject var 🛒: 🛒StoreModel
+    @Environment(\.scenePhase) var scenePhase
     @State private var ⓐpp: 📣MyApp = .pickUpAppWithout(.TapWeight)
     func body(content: Content) -> some View {
         content
             .sheet(isPresented: $🛒.🚩showADSheet) {
                 📣ADView(self.ⓐpp, second: 7)
             }
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    🛒.checkToShowADSheet()
+            .onChange(of: self.scenePhase) {
+                if $0 == .active {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        🛒.checkToShowADSheet()
+                    }
                 }
             }
     }
